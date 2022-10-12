ac3b05d4c6b26a13e565a2088c0f27ffcb91349d
NickBwalley <nickbwalley@gmail.com>
2022-10-12 16:20:48 +0300

    just few mod

--- No1.py
+++ No1.py
@@ -1,4 +1,9 @@
 import networkx as nx 
 import pandas as pd
-N = {'sports complex', 'siwaka', 'phase1B', 'phase1A'}
-D = [('sports complex', 'siwaka', 450), ('siwaka', 'phase1B', 230), ('siwaka', 'phase1A', 10)]
+V = {'sports complex', 'siwaka', 'phase1B', 'phase1A'}
+E = [('sports complex', 'siwaka', 450), ('siwaka', 'phase1B', 230), ('siwaka', 'phase1A', 10)]
+G = nx.DiGraph()
+G.add_nodes_from(V)
+G.add_weighted_edges_from(E)
+# G.nodes
+# G.edges
