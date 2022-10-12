e62924006fcf3bdda04b7368084328b56f99a002
NickBwalley <nickbwalley@gmail.com>
2022-10-12 16:16:06 +0300

    first commit

---
+++ No1.py
@@ -0,0 +1,4 @@
+import networkx as nx 
+import pandas as pd
+N = {'sports complex', 'siwaka', 'phase1B', 'phase1A'}
+D = [('sports complex', 'siwaka', 450), ('siwaka', 'phase1B', 230), ('siwaka', 'phase1A', 10)]
