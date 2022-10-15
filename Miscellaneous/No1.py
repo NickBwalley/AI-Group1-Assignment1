583504927a062b6822b3e9846a966c53c2f142ef
NickBwalley <nickbwalley@gmail.com>
2022-10-16 00:21:05 +0300

    Greedy BFS, UCS implemented

--- Miscellaneous/No1.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx 
-import pandas as pd
-V = {'sports complex', 'siwaka', 'phase1B', 'phase1A'}
-E = [('sports complex', 'siwaka', 450), ('siwaka', 'phase1B', 230), ('siwaka', 'phase1A', 10)]
-G = nx.DiGraph()
-G.add_nodes_from(V)
-G.add_weighted_edges_from(E)
-# G.nodes
-# G.edges
